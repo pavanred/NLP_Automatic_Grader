b266b96e707236063892b175062b35987ceed8e1
pavanred <pavankumar.kh@gmail.com>
2013-03-24 19:36:31 -0500

    evaluation package - weighted final grading

---
+++ src/cs421/autograder/evaluation/Score.java
@@ -0,0 +1,92 @@
+package cs421.autograder.evaluation;
+
+public class Score {
+
+	//Syntax/Grammar
+	private int wordOrderScore; 				//1a
+	private int subjectVerbAgreementScore;		//1b
+	private int verbUsageScore;					//1c
+	private int sentenceFormationScore;			//1d
+	
+	//Semantics (meaning) / Pragmatics (general quality)
+	private int coherenceScore;					//2a
+	private int topicAdherenceScore;			//2b
+	
+	//Length of the essay
+	private int essayLengthScore;				//3a
+	
+	public void setWordOrderScore(int score) {		
+		this.wordOrderScore = score;
+	}
+	
+	public int getWordOrderScore() {		
+		return this.wordOrderScore;
+	}
+
+	public void setSubjectVerbAgreementScore(int score) {
+		this.subjectVerbAgreementScore = score;		
+	}
+	
+	public int getSubjectVerbAgreementScore() {		
+		return this.subjectVerbAgreementScore;
+	}
+
+	public void setVerbUsageScore(int score) {
+		this.verbUsageScore = score;		
+	}
+	
+	public int getVerbUsageScore() {		
+		return this.verbUsageScore;
+	}
+
+	public void setSentenceFormationScore(int score) {
+		this.sentenceFormationScore = score;			
+	}	
+	
+	public int getSentenceFormationScore() {		
+		return this.sentenceFormationScore;
+	}
+
+	public void setCoherenceScore(int score) {
+		this.coherenceScore = score;
+	}
+	
+	public int getCoherenceScore() {		
+		return this.coherenceScore;
+	}
+
+	public void setTopicAdherenceScore(int score) {
+		this.topicAdherenceScore = score;		
+	}
+	
+	public int getTopicAdherenceScore() {		
+		return this.topicAdherenceScore;
+	}
+
+	public void setEssayLengthScore(int score) {
+		this.essayLengthScore = score;		
+	}
+	
+	public int getEssayLengthScore() {		
+		return this.essayLengthScore;
+	}
+
+	public float getFinalScore() {
+		
+		float finalScore = 0.0f;
+		
+		int weightedSyntaxScore = 
+				this.getWordOrderScore() + this.getSubjectVerbAgreementScore() 
+				+ this.getVerbUsageScore() + (2 * this.getSentenceFormationScore());
+		
+		int weightedSemanticsScore = 
+				this.getCoherenceScore() + (3 * this.getTopicAdherenceScore());		
+		
+		finalScore = 
+				(weightedSyntaxScore + weightedSemanticsScore + this.getEssayLengthScore())/10.0f;
+	
+		//rounding to the nearest 0.5
+		return Math.round(finalScore * 2.0f)/2.0f;
+	}	
+	
+}
---
+++ src/cs421/autograder/evaluation/ScoreTest.java
@@ -0,0 +1,47 @@
+package cs421.autograder.evaluation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ScoreTest {
+
+	private float epsilon = 0.0001f;
+	
+	@Test
+	public void finalScoreWithoutRoundingTest() {
+		
+		Score score  = new Score();
+		
+		score.setWordOrderScore(2);
+		score.setSubjectVerbAgreementScore(2);
+		score.setVerbUsageScore(1);
+		score.setSentenceFormationScore(2);
+		
+		score.setCoherenceScore(4);
+		score.setTopicAdherenceScore(2);
+		
+		score.setEssayLengthScore(1);
+		
+		assertEquals(2.0, score.getFinalScore(),epsilon);
+	}
+	
+	@Test
+	public void finalScoreWithRoundingTest() {
+		
+		Score score  = new Score();
+		
+		score.setWordOrderScore(3);
+		score.setSubjectVerbAgreementScore(3);
+		score.setVerbUsageScore(3);
+		score.setSentenceFormationScore(3);
+		
+		score.setCoherenceScore(3);
+		score.setTopicAdherenceScore(1);
+		
+		score.setEssayLengthScore(3);
+		
+		assertEquals(2.5, score.getFinalScore(),epsilon);
+	}
+
+}
